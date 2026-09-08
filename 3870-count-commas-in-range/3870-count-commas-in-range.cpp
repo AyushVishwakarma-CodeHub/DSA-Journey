0f1cf67e05d84312bdbd15bc8c8fa16ac163e8f3
Ayush Raj <ayushvishwakarmadto29@gmail.com>
2026-09-09 01:11:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.7%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int totalCommas = 0;
-
-        for(int i = 1; i <= n; ++i){
-            string s = to_string(i);
-            int len = s.length();
-
-            totalCommas += (len - 1)/3;
-        }
-        return totalCommas;
+        return max(0, n - 999);
     }
 };
